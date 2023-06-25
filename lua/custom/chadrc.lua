4d0878a733cc20ab6746ee00f1bba36ca4419d91
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2023-06-26 02:12:36 +0800

    update config

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -1,32 +1,11 @@
 ---@type ChadrcConfig 
- local M = {}
- M.ui = {
+local M = {}
+
+M.ui = {
   theme = 'tokyodark',
-  nvdash = {
-    load_on_startup = true,
-
-    header = {
-      "           ▄ ▄                   ",
-      "       ▄   ▄▄▄     ▄ ▄▄▄ ▄ ▄     ",
-      "       █ ▄ █▄█ ▄▄▄ █ █▄█ █ █     ",
-      "    ▄▄ █▄█▄▄▄█ █▄█▄█▄▄█▄▄█ █     ",
-      "  ▄ █▄▄█ ▄ ▄▄ ▄█ ▄▄▄▄▄▄▄▄▄▄▄▄▄▄  ",
-      "  █▄▄▄▄ ▄▄▄ █ ▄ ▄▄▄ ▄ ▄▄▄ ▄ ▄ █ ▄",
-      "▄ █ █▄█ █▄█ █ █ █▄█ █ █▄█ ▄▄▄ █ █",
-      "█▄█ ▄ █▄▄█▄▄█ █ ▄▄█ █ ▄ █ █▄█▄█ █",
-      "    █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█ █▄█▄▄▄█    ",
-    },
-
-    buttons = {
-      { "  Find File", "Spc f f", "Telescope find_files" },
-      { "󰈚  Recent Files", "Spc f o", "Telescope oldfiles" },
-      { "󰈭  Find Word", "Spc f w", "Telescope live_grep" },
-      { "  Bookmarks", "Spc m a", "Telescope marks" },
-      { "  Themes", "Spc t h", "Telescope themes" },
-      { "  Mappings", "Spc c h", "NvCheatsheet" },
-    },
-   },
 }
- M.plugins = 'custom.plugins'
- M.mappings = require "custom.mappings"
- return M
+
+M.plugins = 'custom.plugins'
+M.mappings = require "custom.mappings"
+
+return M
--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -1,9 +1,16 @@
 local mappings = {}
 
-mappings.abc = {
+mappings.disabled = {
+  n = {
+    -- ["C-n"] = "",
+  }
+}
+
+mappings.custom = {
   n = {
     ["<C-d>"] = { "<C-d>zz", "go down half screen" },
     ["<C-u>"] = { "<C-u>zz", "go up half screen" },
+    -- ["<C-n>"] = { "<cmd> NvimTreeToggle <CR>", "Toggle nvimtree" },
   }
 }
 

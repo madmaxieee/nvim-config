7102a7e8e7d95f56925e2bbb8fe8a79569fca7c0
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2023-07-05 15:53:17 +0800

    add flash.nvim

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -28,13 +28,60 @@ local plugins = {
   },
 
   -- extra plugins
+  -- {
+  --   "ggandor/leap.nvim",
+  --   -- leap.nvim lazy loads itself
+  --   lazy = false,
+  --   config = function()
+  --     require("leap").add_default_mappings(true)
+  --   end,
+  -- },
   {
-    "ggandor/leap.nvim",
-    -- leap.nvim lazy loads itself
-    lazy = false,
-    config = function()
-      require("leap").add_default_mappings(true)
-    end,
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    opts = {},
+    keys = {
+      {
+        "s",
+        mode = { "n", "x", "o" },
+        function()
+          require("flash").jump()
+        end,
+        desc = "Flash",
+      },
+      {
+        "S",
+        mode = { "n", "o", "x" },
+        function()
+          require("flash").treesitter()
+        end,
+        desc = "Flash Treesitter",
+      },
+      {
+        "r",
+        mode = "o",
+        function()
+          require("flash").remote()
+        end,
+        desc = "Remote Flash",
+      },
+      {
+        "R",
+        mode = { "o", "x" },
+        function()
+          require("flash").treesitter_search()
+        end,
+        desc = "Flash Treesitter Search",
+      },
+      {
+        "<c-s>",
+        mode = { "c" },
+        function()
+          require("flash").toggle()
+        end,
+        desc = "Toggle Flash Search",
+      },
+    },
   },
 
   {

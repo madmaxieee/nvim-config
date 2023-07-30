0f82bfa7575245b139e5c5beecf577a98ba1b102
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2023-07-30 17:14:57 +0800

    configure comment string for dofiles

--- lua/custom/autocmd.lua
+++ lua/custom/autocmd.lua
@@ -87,3 +87,27 @@ end
 for pattern, ft in pairs(fileDetectionMap) do
   set_filetype(pattern, ft)
 end
+
+-- ========================
+-- configure commentstring
+-- ========================
+
+local commentstringGroup = vim.api.nvim_create_augroup("Commentstring", { clear = true })
+
+local commentstringMap = {
+  ["*.dof"] = "// %s",
+}
+
+local set_commentstring = function(pattern, commentstring)
+  vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+    group = commentstringGroup,
+    pattern = pattern,
+    callback = function()
+      vim.bo.commentstring = commentstring
+    end,
+  })
+end
+
+for pattern, commentstring in pairs(commentstringMap) do
+  set_commentstring(pattern, commentstring)
+end

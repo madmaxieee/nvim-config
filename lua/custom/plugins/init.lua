7ea93473d0e12b50c0ec8f5e0a6a53ffa341dfd2
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2023-08-09 13:28:05 +0800

    open nvim tree in floating window

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -66,6 +66,31 @@ local plugins = {
           show = { git = true },
         },
       },
+      view = {
+        float = {
+          enable = true,
+          open_win_config = function()
+            local HEIGHT_RATIO = 0.7
+            local WIDTH_RATIO = 0.4
+            local screen_w = vim.opt.columns:get()
+            local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+            local window_w = screen_w * WIDTH_RATIO
+            local window_h = screen_h * HEIGHT_RATIO
+            local window_w_int = math.floor(window_w)
+            local window_h_int = math.floor(window_h)
+            local center_x = (screen_w - window_w) / 2
+            local center_y = ((vim.opt.lines:get() - window_h) / 2) - vim.opt.cmdheight:get()
+            return {
+              border = "rounded",
+              relative = "editor",
+              row = center_y,
+              col = center_x,
+              width = window_w_int,
+              height = window_h_int,
+            }
+          end,
+        },
+      },
     },
   },
 

cdfd1075340855687e3a725e8906cf81557cc4a1
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2023-08-10 22:54:05 +0800

    fix statuscol numbers

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -221,9 +221,13 @@ local plugins = {
       require("statuscol").setup {
         relculright = true,
         segments = {
-          { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
           { text = { "%s" }, click = "v:lua.ScSa" },
-          { text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
+          { text = { builtin.lnumfunc }, click = "v:lua.ScLa" },
+          {
+            text = { " ", builtin.foldfunc, " " },
+            condition = { builtin.not_empty, true, builtin.not_empty },
+            click = "v:lua.ScFa",
+          },
         },
         ft_ignore = { "terminal", "NvimTree" },
       }

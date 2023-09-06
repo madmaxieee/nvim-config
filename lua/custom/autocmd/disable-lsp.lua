97608ad74e731c3227fff9ad6c86ad437ff342be
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2023-09-06 20:03:26 +0800

    update autocmd for .env

--- lua/custom/autocmd/disable-lsp.lua
+++ lua/custom/autocmd/disable-lsp.lua
@@ -1,20 +1,8 @@
 -- disable lsp diagnostic for certain file names
-local disable_lsp_group = vim.api.nvim_create_augroup("DisableLsp", { clear = true })
-
-local patterns = {
-  ".env*",
-}
-
-local function disable_lsp(pattern)
-  vim.api.nvim_create_autocmd("BufEnter", {
-    group = disable_lsp_group,
-    pattern = pattern,
-    callback = function()
-      vim.diagnostic.disable()
-    end,
-  })
-end
-
-for _, pattern in ipairs(patterns) do
-  disable_lsp(pattern)
-end
+vim.api.nvim_create_autocmd("BufEnter", {
+  group = vim.api.nvim_create_augroup("DisableLsp", { clear = true }),
+  pattern = { ".env*" },
+  callback = function()
+    vim.diagnostic.disable()
+  end,
+})

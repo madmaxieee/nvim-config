b33fd255fa4c8335c6efdc7f2a1d650bc35ce813
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2023-09-08 16:46:08 +0800

    use my own fork of code shot

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -586,51 +586,29 @@ local plugins = {
   },
 
   {
-    enabled = false,
-
-    "vhyrro/hologram.nvim",
-    ft = { "markdown" },
+    "madmaxieee/code-shot.nvim",
+    cmd = { "CodeShot" },
     config = function()
-      require("hologram").setup {
-        auto_display = true, -- WIP automatic markdown image display, may be prone to breaking
-      }
-    end,
-  },
-
-  {
-    enabled = false,
-    "3rd/image.nvim",
-    ft = { "markdown" },
-    config = function()
-      -- to load lua modules installed from luarock
-      package.path = package.path .. ";" .. vim.fn.expand "$HOME" .. "/.luarocks/share/lua/5.1/?/init.lua;"
-      package.path = package.path .. ";" .. vim.fn.expand "$HOME" .. "/.luarocks/share/lua/5.1/?.lua;"
-
-      require("image").setup {
-        backend = "kitty",
-        integrations = {
-          markdown = {
-            enabled = true,
-            sizing_strategy = "auto",
-            download_remote_images = true,
-            clear_in_insert_mode = false,
-            only_render_image_at_cursor = false,
-          },
-          -- neorg = {
-          --   enabled = true,
-          --   download_remote_images = true,
-          --   clear_in_insert_mode = false,
-          --   only_render_image_at_cursor = false,
-          -- },
-        },
-        max_width = nil,
-        max_height = nil,
-        max_width_window_percentage = nil,
-        max_height_window_percentage = 50,
-        kitty_method = "normal",
-        kitty_tmux_write_delay = 10, -- makes rendering more reliable with Kitty+Tmux
-        window_overlap_clear_enabled = false, -- toggles images when windows are overlapped
-        window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
+      require("code-shot").setup {
+        to_clipboard = true,
+        ---@return string output file path
+        output = function()
+          local buf_name = vim.api.nvim_buf_get_name(0)
+          return string.match(buf_name, "([^/^%.]+)[^/]*$") .. ".png"
+        end,
+        ---@return string[]
+        -- select_area: {s_start: {row: number, col: number}, s_end: {row: number, col: number}} | nil
+        options = function(select_area)
+          if not select_area then
+            return {}
+          end
+          return {
+            "--line-offset",
+            select_area.s_start.row,
+            "--theme",
+            "Dracula",
+          }
+        end,
       }
     end,
   },

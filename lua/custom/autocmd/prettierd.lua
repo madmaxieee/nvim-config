db38eb861400648c9a93ac20cf408ca71383747f
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2023-09-14 10:24:50 +0800

    don't run prettierd if it does not exist

--- lua/custom/autocmd/prettierd.lua
+++ lua/custom/autocmd/prettierd.lua
@@ -1,6 +1,8 @@
-vim.api.nvim_create_autocmd({ "VimEnter" }, {
-  group = vim.api.nvim_create_augroup("prettierd", { clear = true }),
-  callback = function()
-    vim.fn.jobstart { "prettierd", "restart" }
-  end,
-})
+if vim.fn.executable "prettierd" then
+  vim.api.nvim_create_autocmd({ "VimEnter" }, {
+    group = vim.api.nvim_create_augroup("prettierd", { clear = true }),
+    callback = function()
+      vim.fn.jobstart { "prettierd", "restart" }
+    end,
+  })
+end

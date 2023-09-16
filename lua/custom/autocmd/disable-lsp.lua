f8bb3b4d7740a513e1b5cc8491cb1db9cbab3206
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2023-09-16 23:56:31 +0800

    disable diagnostic on entering .env file buffer and re-enabling it on leaving the buffer

--- lua/custom/autocmd/disable-lsp.lua
+++ lua/custom/autocmd/disable-lsp.lua
@@ -1,8 +1,18 @@
 -- disable lsp diagnostic for certain file names
--- vim.api.nvim_create_autocmd("BufEnter", {
---   group = vim.api.nvim_create_augroup("DisableLsp", { clear = true }),
---   pattern = { ".env*" },
---   callback = function()
---     vim.diagnostic.disable()
---   end,
--- })
+local disable_lsp_group = vim.api.nvim_create_augroup("DisableLsp", { clear = true })
+
+vim.api.nvim_create_autocmd("BufEnter", {
+  group = disable_lsp_group,
+  pattern = { ".env*" },
+  callback = function()
+    vim.diagnostic.disable()
+  end,
+})
+
+vim.api.nvim_create_autocmd("BufLeave", {
+  group = disable_lsp_group,
+  pattern = { ".env*" },
+  callback = function()
+    vim.diagnostic.enable()
+  end,
+})

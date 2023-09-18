f184da31d643fa6708031cbdbd194b8473071c9b
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2023-09-19 01:32:49 +0800

    use underline cursor in visual mode

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -9,6 +9,7 @@ vim.opt.foldlevel = 99
 vim.opt.foldlevelstart = 99
 vim.opt.foldenable = true
 vim.opt.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+vim.opt.guicursor = "n-c-sm:block,i-ci-ve:ver25,v-r-cr-o:hor20"
 
 require "custom.usercmd"
 require "custom.autocmd"

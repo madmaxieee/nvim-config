1335174f57660930191d571815e8b5fb31db8728
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2023-09-19 01:32:38 +0800

    dont enable relative number if not enabled originally

--- lua/custom/autocmd/relative-line.lua
+++ lua/custom/autocmd/relative-line.lua
@@ -1,17 +1,24 @@
 -- disable relative line numbers in command mode
 local relative_line_group = vim.api.nvim_create_augroup("RelativeLine", { clear = true })
 
+local relative_line_enabled = false
+
 vim.api.nvim_create_autocmd({ "CmdLineEnter" }, {
   group = relative_line_group,
   callback = function()
-    vim.wo.relativenumber = false
-    vim.api.nvim_command "redraw"
+    relative_line_enabled = vim.wo.relativenumber
+    if relative_line_enabled then
+      vim.wo.relativenumber = false
+      vim.api.nvim_command "redraw"
+    end
   end,
 })
 
 vim.api.nvim_create_autocmd({ "CmdLineLeave" }, {
   group = relative_line_group,
   callback = function()
-    vim.wo.relativenumber = true
+    if relative_line_enabled then
+      vim.wo.relativenumber = true
+    end
   end,
 })

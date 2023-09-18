2727c12bd93bd8dce1b842616752cc7cd61854b6
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2023-09-19 02:52:28 +0800

    allow Q and Qa to accept bang

--- lua/custom/usercmd.lua
+++ lua/custom/usercmd.lua
@@ -1,5 +1,5 @@
-vim.api.nvim_create_user_command("Q", "q", {})
-vim.api.nvim_create_user_command("Qa", "qa", {})
+vim.api.nvim_create_user_command("Q", "q<bang>", { bang = true })
+vim.api.nvim_create_user_command("Qa", "qa<bang>", { bang = true })
 vim.api.nvim_create_user_command("W", "w", {})
 vim.api.nvim_create_user_command("Wq", "wq", {})
 vim.api.nvim_create_user_command("Wqa", "wqa", {})

01efd559f5df00557d89623a2f7103d61a7dcf8e
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2023-10-03 14:27:15 +0800

    update comment string autocmd

--- lua/custom/autocmd/comment.lua
+++ lua/custom/autocmd/comment.lua
@@ -1,7 +1,7 @@
 -- configure commentstring
 local commentstring_group = vim.api.nvim_create_augroup("Commentstring", { clear = true })
 
-local commentstring_map = {
+local commentstring_pattern_map = {
   ["*.dof"] = "// %s",
 }
 
@@ -15,6 +15,25 @@ local set_commentstring = function(pattern, commentstring)
   })
 end
 
-for pattern, string in pairs(commentstring_map) do
+for pattern, string in pairs(commentstring_pattern_map) do
   set_commentstring(pattern, string)
+end
+
+local commentstring_filetype_map = {
+  kdl = "// %s",
+  kitty = "# %s",
+}
+
+local set_commentstring_filetype = function(filetype, commentstring)
+  vim.api.nvim_create_autocmd({ "FileType" }, {
+    group = commentstring_group,
+    pattern = filetype,
+    callback = function()
+      vim.bo.commentstring = commentstring
+    end,
+  })
+end
+
+for filetype, string in pairs(commentstring_filetype_map) do
+  set_commentstring_filetype(filetype, string)
 end

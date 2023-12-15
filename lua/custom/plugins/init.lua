f2547843ae4e350ca0c15920a849b93b55240de7
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2023-12-15 13:37:21 +0800

    add zk

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -626,6 +626,28 @@ local plugins = {
   },
 
   {
+    "mickael-menu/zk-nvim",
+    dependencies = { "nvim-telescope/telescope.nvim" },
+    event = "VeryLazy",
+    config = function()
+      require("zk").setup {
+        picker = "telescope",
+        lsp = {
+          -- `config` is passed to `vim.lsp.start_client(config)`
+          config = {
+            cmd = { "zk", "lsp" },
+            name = "zk",
+          },
+          auto_attach = {
+            enabled = true,
+            filetypes = { "markdown" },
+          },
+        },
+      }
+    end,
+  },
+
+  {
     enabled = false,
     "folke/noice.nvim",
     event = "VeryLazy",

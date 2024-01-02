94db09d183c829401b0254e3e5a3c1f6643e63c5
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2024-01-03 00:17:09 +0800

    add bindings for workspace symbols and diagnostics

--- lua/custom/mappings/telescope.lua
+++ lua/custom/mappings/telescope.lua
@@ -1,7 +1,9 @@
 local telescope_mappings = {
   n = {
     ["<leader>fg"] = { "<cmd> Telescope git_files <CR>", "Find git files" },
+    ["<leader>fd"] = { "<cmd> Telescope diagnostics <CR>", "Find diagnostic" },
     ["<leader>fs"] = { "<cmd> Telescope lsp_document_symbols <CR>", "Search document symbols" },
+    ["<leader>ws"] = { "<cmd> Telescope lsp_dynamic_workspace_symbols <CR>", "Search workspace symbols" },
     ["<leader>fm"] = { "<cmd> Telescope marks <CR>", "Search marks" },
     ["<leader>fj"] = { "<cmd> Telescope jumplist <CR>", "Search jumplist" },
     ["<leader>gs"] = { "<cmd> Telescope git_status <CR>", "Git status" },

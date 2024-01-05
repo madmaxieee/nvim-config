45b3a881c4379ea901f0878133fe932c94946abc
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2024-01-05 18:22:15 +0800

    fix: fix treesitter config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,21 +1,3 @@
-local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-parser_config.typst = {
-  install_info = {
-    -- local path or git repo
-    url = vim.fn.stdpath "config" .. "/tree-sitter-typst",
-    -- note that some parsers also require src/scanner.c or src/scanner.cc
-    files = { "src/parser.c", "src/scanner.c" },
-    -- optional entries:
-    -- default branch in case of git repo if different from master
-    branch = "master",
-    -- if stand-alone parser without npm dependencies
-    generate_requires_npm = false,
-    -- if folder contains pre-generated src/parser.c
-    requires_generate_from_grammar = false,
-  },
-  filetype = "typst",
-}
-
 return {
   "nvim-treesitter/nvim-treesitter",
   cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
@@ -59,6 +41,23 @@ return {
     indent = { enable = true },
   },
   config = function(_, opts)
+    local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+    parser_config.typst = {
+      install_info = {
+        -- local path or git repo
+        url = vim.fn.stdpath "config" .. "/tree-sitter-typst",
+        -- note that some parsers also require src/scanner.c or src/scanner.cc
+        files = { "src/parser.c", "src/scanner.c" },
+        -- optional entries:
+        -- default branch in case of git repo if different from master
+        branch = "master",
+        -- if stand-alone parser without npm dependencies
+        generate_requires_npm = false,
+        -- if folder contains pre-generated src/parser.c
+        requires_generate_from_grammar = false,
+      },
+      filetype = "typst",
+    }
     require("nvim-treesitter.configs").setup(opts)
   end,
 }

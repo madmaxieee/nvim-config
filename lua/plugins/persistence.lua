734f0bdc8dad07a6dbf3733126c02a41fb3dbd03
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2024-01-06 01:51:25 +0800

    feat: add persistence

---
+++ lua/plugins/persistence.lua
@@ -0,0 +1,43 @@
+local persistence_group = vim.api.nvim_create_augroup("Persistence", { clear = true })
+local home = vim.fn.expand "~"
+local disabled_dirs = {
+  [home] = true,
+  [home .. "/Downloads"] = true,
+  ["/private/tmp"] = true,
+}
+
+-- disable persistence for certain directories
+vim.api.nvim_create_autocmd({ "VimEnter" }, {
+  group = persistence_group,
+  callback = function()
+    local cwd = vim.fn.getcwd()
+    if vim.fn.argc() == 0 and not vim.g.started_with_stdin and not disabled_dirs[cwd] then
+      require("persistence").load()
+    else
+      require("persistence").stop()
+    end
+  end,
+  nested = true,
+})
+
+-- disable persistence if nvim started with stdin
+vim.api.nvim_create_autocmd({ "StdinReadPre" }, {
+  group = persistence_group,
+  callback = function()
+    vim.g.started_with_stdin = true
+  end,
+})
+
+return {
+  "folke/persistence.nvim",
+  event = "BufReadPre",
+  opts = {
+    -- directory where session files are saved
+    dir = vim.fn.expand(vim.fn.stdpath "state" .. "/sessions/"),
+    -- sessionoptions used for saving
+    options = { "buffers", "curdir", "tabpages", "winsize" },
+    -- a function to call before saving the session
+    pre_save = nil,
+  },
+  config = true,
+}

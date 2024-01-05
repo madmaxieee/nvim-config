20a8dfae8edb3212174bd673657dcd97d9914f0c
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2024-01-06 05:58:33 +0800

    make lazygit binding better'

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -2,25 +2,30 @@ local map = require("utils").safe_keymap_set
 
 return {
   "akinsho/toggleterm.nvim",
-  keys = {
-    "<A-i>",
-    "<leader>lg",
-  },
+  event = "VeryLazy",
+  keys = { "<A-i>" },
   version = "*",
-  opts = {
-    open_mapping = "<A-i>",
-    direction = "float",
-    float_opts = {
-      border = "rounded",
-    },
-  },
-  config = function(_, opts)
-    require("toggleterm").setup(opts)
+  config = function()
     local Terminal = require("toggleterm.terminal").Terminal
-    local lazygit = Terminal:new { cmd = "lazygit", hidden = true }
+    local lazygit = Terminal:new {
+      cmd = "exec lazygit",
+      direction = "float",
+      hidden = true,
+      on_open = function(term)
+        vim.api.nvim_buf_set_keymap(term.bufnr, "t", "q", "<cmd>close<CR>", { noremap = true, silent = true })
+      end,
+    }
 
     map("n", "<leader>lg", function()
       lazygit:toggle()
-    end, { desc = "Toggle terminal" })
+    end, { desc = "Toggle LazyGit" })
+
+    require("toggleterm").setup {
+      open_mapping = "<A-i>",
+      direction = "float",
+      float_opts = {
+        border = "rounded",
+      },
+    }
   end,
 }

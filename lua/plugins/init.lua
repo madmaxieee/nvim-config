af19521b4fa58977d8a8bb0fcf46a3c2e9a3ecf8
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2024-01-06 04:55:05 +0800

    feat: add barbecue

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -59,4 +59,12 @@ return {
     event = "VeryLazy",
     opts = {},
   },
+  {
+    "utilyre/barbecue.nvim",
+    lazy = false,
+    dependencies = { "SmiteshP/nvim-navic", "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("barbecue").setup {}
+    end,
+  },
 }

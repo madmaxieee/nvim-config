78bbeb40af50d4777613dcda5e37328686868d10
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2024-01-07 03:41:36 +0800

    configure lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -7,6 +7,59 @@ local function show_macro_recording()
   end
 end
 
+---@diagnostic disable-next-line: unused-local, unused-function
+local function show_git_status()
+  if not vim.b.gitsigns_head or vim.b.gitsigns_git_status then
+    return ""
+  end
+
+  local git_status = vim.b.gitsigns_status_dict
+
+  local added = (git_status.added and git_status.added ~= 0) and ("  " .. git_status.added) or ""
+  local changed = (git_status.changed and git_status.changed ~= 0) and ("  " .. git_status.changed) or ""
+  local removed = (git_status.removed and git_status.removed ~= 0) and ("  " .. git_status.removed) or ""
+  local branch_name = "  " .. git_status.head
+
+  return branch_name .. added .. changed .. removed
+end
+
+local function show_branch()
+  if not vim.b.gitsigns_head or vim.b.gitsigns_git_status then
+    return ""
+  end
+
+  local git_status = vim.b.gitsigns_status_dict
+
+  return "  " .. git_status.head
+end
+
+local function show_lsp_status()
+  for _, client in ipairs(vim.lsp.get_clients()) do
+    if client.attached_buffers[vim.api.nvim_get_current_buf()] and client.name ~= "null-ls" then
+      return (vim.o.columns > 100 and "   LSP ~ " .. client.name) or "   LSP "
+    end
+  end
+end
+
+local function show_file_info()
+  local icon = " 󰈚 "
+  local path = vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf())
+  local name = (path == "" and "Empty ") or path:match "([^/\\]+)[/\\]*$"
+
+  if name ~= "Empty " then
+    local devicons_present, devicons = pcall(require, "nvim-web-devicons")
+
+    if devicons_present then
+      local ft_icon = devicons.get_icon(name)
+      icon = (ft_icon ~= nil and " " .. ft_icon) or icon
+    end
+
+    name = " " .. name
+  end
+
+  return icon .. name
+end
+
 return {
   "nvim-lualine/lualine.nvim",
   lazy = false,
@@ -14,35 +67,36 @@ return {
   opts = {
     options = {
       globalstatus = true,
-      section_separators = " ",
+      section_separators = "",
       refresh = {
         statusline = 250,
         tabline = 1000,
         winbar = 1000,
       },
-      component_separators = "▕",
+      component_separators = "▕%##",
     },
     sections = {
       lualine_a = {
         "mode",
-        {
-          "macro-recording",
-          fmt = show_macro_recording,
-        },
+        { "macro-recording", fmt = show_macro_recording },
       },
       lualine_b = {
-        "branch",
-        -- "diff",
+        { "file-info", fmt = show_file_info },
+      },
+      lualine_c = {
+        { "_branch", fmt = show_branch },
         "diagnostics",
       },
-      lualine_c = { "filename" },
       lualine_x = {
-        "encoding",
-        -- "fileformat",
+        { "lsp-status", fmt = show_lsp_status },
         "filetype",
       },
-      lualine_y = { "progress" },
-      lualine_z = { "location" },
+      lualine_y = {
+        "progress",
+      },
+      lualine_z = {
+        "location",
+      },
     },
   },
 }

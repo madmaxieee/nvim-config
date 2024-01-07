cb6303a98ede94e7aa197c11b122b0aeb3aa96a0
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2024-01-08 03:42:22 +0800

    remove unused keymaps for nvim-treesitter-textobject

--- lua/plugins/nvim-treesitter-textobjects.lua
+++ lua/plugins/nvim-treesitter-textobjects.lua
@@ -15,10 +15,6 @@ return {
     "[z",
     "[M",
     "[]",
-    "<leader>s",
-    "<leader>S",
-    "<leader>df",
-    "<leader>dF",
   },
   dependencies = {
     -- "nvim-treesitter/nvim-treesitter"
@@ -33,9 +29,7 @@ return {
           goto_next_start = {
             ["]m"] = "@function.outer",
             ["]]"] = { query = "@class.outer", desc = "Next class start" },
-
             ["]o"] = "@loop.*",
-
             ["]s"] = { query = "@scope", query_group = "locals", desc = "Next scope" },
             ["]z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
           },
@@ -46,9 +40,7 @@ return {
           goto_previous_start = {
             ["[m"] = "@function.outer",
             ["[["] = "@class.outer",
-
             ["[o"] = "@loop.*",
-
             ["[s"] = { query = "@scope", query_group = "locals", desc = "Next scope" },
             ["[z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
           },
@@ -56,30 +48,9 @@ return {
             ["[M"] = "@function.outer",
             ["[]"] = "@class.outer",
           },
-          -- goto_next = {
-          --   ["]d"] = "@conditional.outer",
-          -- },
-          -- goto_previous = {
-          --   ["[d"] = "@conditional.outer",
-          -- },
         },
-        swap = {
-          enable = true,
-          swap_next = {
-            ["<leader>s"] = "@parameter.inner",
-          },
-          swap_previous = {
-            ["<leader>S"] = "@parameter.inner",
-          },
-        },
-        lsp_interop = {
-          enable = false,
-          floating_preview_opts = {},
-          peek_definition_code = {
-            ["<leader>df"] = "@function.outer",
-            ["<leader>dF"] = "@class.outer",
-          },
-        },
+        swap = { enable = false },
+        lsp_interop = { enable = false },
       },
     }
   end,

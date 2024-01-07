886586e2a3e8eb408e8e50c29bed09ae0acc5f4a
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2024-01-08 03:43:08 +0800

    hijack netrw with neotree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,11 +1,15 @@
+local is_opening_dir = vim.fn.argc() > 0 and vim.fn.isdirectory(vim.fn.argv()[0])
+
 return {
+  -- don't lazy load if opening a directory, since we want to hijack netrw
+  lazy = not is_opening_dir,
   "nvim-neo-tree/neo-tree.nvim",
   cmd = "Neotree",
   keys = {
     {
       "<C-n>",
       mode = "n",
-      "<cmd> Neotree toggle position=float <CR>",
+      "<cmd> Neotree toggle <CR>",
       desc = "open NeoTree",
     },
   },
@@ -16,6 +20,12 @@ return {
     "MunifTanjim/nui.nvim",
     "3rd/image.nvim",
   },
+  init = function()
+    -- disable netrw
+    vim.g.loaded_netrwPlugin = true
+    vim.g.loaded_netrw = true
+    vim.g.loaded_netrw_gitignore = true
+  end,
   opts = {
     default_component_configs = {
       git_status = {
@@ -33,6 +43,12 @@ return {
           conflict = "=",
         },
       },
+    },
+    filesystem = {
+      hijack_netrw_behavior = "open_default",
+    },
+    window = {
+      position = "float",
     },
   },
 }

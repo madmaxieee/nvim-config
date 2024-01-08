b1554b5b3ce6c4f6013a548e8321faccbea5d3a7
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2024-01-08 13:50:28 +0800

    add nvim-surround

---
+++ lua/plugins/nvim-surround.lua
@@ -0,0 +1,20 @@
+return {
+  "kylechui/nvim-surround",
+  event = "VeryLazy",
+  config = function()
+    require("nvim-surround").setup {
+      keymaps = {
+        insert = "<C-g>z",
+        insert_line = "<C-g>Z",
+        normal = "yz",
+        normal_cur = "yzz",
+        normal_line = "yZ",
+        normal_cur_line = "yZZ",
+        visual = "Z",
+        visual_line = "gZ",
+        delete = "dz",
+        change = "cz",
+      },
+    }
+  end,
+}

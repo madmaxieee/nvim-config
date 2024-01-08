0609348d2826d32a0774afe495b027e70be953ab
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2024-01-08 18:32:37 +0800

    resize toggle term when vim is resized

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,42 +1,76 @@
 local map = require("utils").safe_keymap_set
 local float_window_config = require("utils").float_window_config
 
+local terminal = nil
 local lazygit = nil
+
+local function get_terminal_float_opts()
+  return float_window_config(0.8, 0.6, {})
+end
+
+local function get_lazygit_float_opts()
+  return float_window_config(0.8, 0.8, {})
+end
 
 return {
   "akinsho/toggleterm.nvim",
   keys = {
-    "<A-i>",
+    {
+      "<A-i>",
+      mode = "n",
+      function()
+        if terminal then
+          terminal:toggle()
+        end
+      end,
+      desc = "Toggle terminal",
+    },
     {
       "<A-g>",
       mode = "n",
       function()
-        if lazygit == nil then
-          return
+        if lazygit then
+          lazygit:toggle()
         end
-        lazygit:toggle()
       end,
-      desc = "Flash",
+      desc = "Toggle lazygit",
     },
   },
   version = "*",
+  init = function()
+    vim.api.nvim_create_autocmd("VimResized", {
+      group = vim.api.nvim_create_augroup("ToggleTermResize", { clear = true }),
+      callback = function()
+        if terminal then
+          terminal.float_opts = get_terminal_float_opts()
+        end
+        if lazygit then
+          lazygit.float_opts = get_lazygit_float_opts()
+        end
+      end,
+    })
+  end,
   config = function()
     local Terminal = require("toggleterm.terminal").Terminal
+    terminal = Terminal:new {
+      cmd = vim.o.shell,
+      direction = "float",
+      hidden = true,
+      float_opts = get_terminal_float_opts(),
+      on_open = function(term)
+        map("t", "<C-c>", "<cmd>close<CR>", { buffer = term.bufnr })
+        map("t", "<A-i>", "<cmd>close<CR>", { buffer = term.bufnr })
+      end,
+    }
     lazygit = Terminal:new {
       cmd = "exec lazygit",
       direction = "float",
       hidden = true,
-      float_opts = float_window_config(0.8, 0.8, {}),
+      float_opts = get_lazygit_float_opts(),
       on_open = function(term)
         map("t", "<C-c>", "<cmd>close<CR>", { buffer = term.bufnr })
         map("t", "<A-g>", "<cmd>close<CR>", { buffer = term.bufnr })
       end,
-    }
-
-    require("toggleterm").setup {
-      open_mapping = "<A-i>",
-      direction = "float",
-      float_opts = float_window_config(0.8, 0.6, {}),
     }
   end,
 }

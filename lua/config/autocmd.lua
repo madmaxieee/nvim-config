b56fbfd2ddee210c87366e1c279d265dcffc34a9
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2024-01-08 17:54:20 +0800

    reload buffer if file is changed outside of nvim

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -39,3 +39,11 @@ autocmd("LspAttach", {
     end
   end,
 })
+
+-- auto reload files when changed outside of neovim
+vim.api.nvim_create_autocmd("FocusGained", {
+  group = vim.api.nvim_create_augroup("AutoReload", { clear = true }),
+  callback = function()
+    vim.cmd "checktime"
+  end,
+})

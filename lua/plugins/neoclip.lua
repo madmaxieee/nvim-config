a8f56a4b4c03a39b792e33fd911e514cefdb9f5f
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2024-03-22 16:22:25 +0800

    make neoclip paste directly on enter

--- lua/plugins/neoclip.lua
+++ lua/plugins/neoclip.lua
@@ -6,6 +6,24 @@ return {
   config = function()
     require("neoclip").setup {
       enable_persistent_history = true,
+      keys = {
+        telescope = {
+          i = {
+            paste = "<cr>",
+            paste_behind = "<c-k>",
+            replay = "<c-q>", -- replay a macro
+            delete = "<c-d>", -- delete an entry
+            edit = "<c-e>", -- edit an entry
+          },
+          n = {
+            paste = "<cr>",
+            paste_behind = "P",
+            replay = "q",
+            delete = "d",
+            edit = "e",
+          },
+        },
+      },
     }
   end,
 }

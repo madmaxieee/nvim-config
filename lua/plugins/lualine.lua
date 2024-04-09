f527752f3319f491b06f22770a4dea6379743369
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2024-04-10 01:28:41 +0800

    don't show typos_ls in lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -35,7 +35,11 @@ end
 
 local function show_lsp_status()
   for _, client in ipairs(vim.lsp.get_clients()) do
-    if client.attached_buffers[vim.api.nvim_get_current_buf()] and client.name ~= "null-ls" then
+    if
+      client.attached_buffers[vim.api.nvim_get_current_buf()]
+      and client.name ~= "null-ls"
+      and client.name ~= "typos_ls"
+    then
       return (vim.o.columns > 100 and "   LSP ~ " .. client.name) or "   LSP "
     end
   end

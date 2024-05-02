11ab889cf61953c4c18794b2b816186dec48dad7
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2024-05-02 19:16:34 +0800

    ignore .venv file in telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -151,6 +151,7 @@ return {
         },
         file_ignore_patterns = {
           "node_modules/",
+          "%.venv/",
           "%.git/",
           "%.obsidian%-?.*/",
           "lazy%-lock%.json$",

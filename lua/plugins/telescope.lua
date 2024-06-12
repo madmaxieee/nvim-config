9538ecf29fdc261c6db3ebf39a7908904e6f3775
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2024-06-12 21:10:28 +0800

    configure telescope undo

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -189,7 +189,24 @@ return {
           override_file_sorter = true,
           case_mode = "smart_case",
         },
-        undo = {},
+        undo = {
+          mappings = {
+            i = {
+              ["<cr>"] = require("telescope-undo.actions").yank_additions,
+              ["<S-cr>"] = require("telescope-undo.actions").yank_deletions,
+              ["<C-cr>"] = require("telescope-undo.actions").restore,
+              -- alternative defaults, for users whose terminals do questionable things with modified <cr>
+              ["<C-y>"] = require("telescope-undo.actions").yank_deletions,
+              ["<C-r>"] = require("telescope-undo.actions").restore,
+            },
+            n = {
+              ["y"] = require("telescope-undo.actions").yank_additions,
+              ["Y"] = require("telescope-undo.actions").yank_deletions,
+              ["u"] = require("telescope-undo.actions").restore,
+              ["<cr>"] = require("telescope-undo.actions").restore,
+            },
+          },
+        },
       },
     }
 

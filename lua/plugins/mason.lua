1466036eda2b62f69cabb80648905008f159658f
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2024-08-28 00:52:54 +0800

    fix mason config

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,3 +1,42 @@
+local ensure_installed = {
+  "bash-language-server",
+  "beautysh",
+  "black",
+  "checkstyle",
+  "clangd",
+  "cmake-language-server",
+  "codelldb",
+  "css-lsp",
+  "dockerfile-language-server",
+  "eslint-lsp",
+  "gopls",
+  "html-lsp",
+  "isort",
+  "jdtls",
+  "json-lsp",
+  "lua-language-server",
+  "markdownlint",
+  "nil",
+  "nginx-language-server",
+  "pyright",
+  "pylint",
+  "rust-analyzer",
+  "stylua",
+  "svelte-language-server",
+  "taplo",
+  "tailwindcss-language-server",
+  "typescript-language-server",
+  "typos-lsp",
+  "typst-lsp",
+  "yaml-language-server",
+  "xmlformatter",
+  "zls",
+}
+
+vim.api.nvim_create_user_command("MasonInstallAll", function()
+  vim.cmd("MasonInstall " .. table.concat(ensure_installed, " "))
+end, {})
+
 return {
   "williamboman/mason.nvim",
   build = function(_)
@@ -12,52 +51,10 @@ return {
     "MasonLog",
   },
   opts = {
-    ensure_installed = {
-      "bash-language-server",
-      "beautysh",
-      "black",
-      "checkstyle",
-      "clangd",
-      "cmake-language-server",
-      "codelldb",
-      "css-lsp",
-      "dockerfile-language-server",
-      "eslint-lsp",
-      "gopls",
-      "html-lsp",
-      "isort",
-      "jdtls",
-      "json-lsp",
-      "lua-language-server",
-      "markdownlint",
-      "nginx-language-server",
-      "pyright",
-      "pylint",
-      "rust-analyzer",
-      "stylua",
-      "svelte-language-server",
-      "taplo",
-      "tailwindcss-language-server",
-      "typescript-language-server",
-      "typos-lsp",
-      "typst-lsp",
-      "yaml-language-server",
-      "xmlformatter",
-      "zls",
-    },
-
     PATH = "prepend",
-
     max_concurrent_installers = 10,
   },
   config = function(_, opts)
     require("mason").setup(opts)
-
-    -- custom nvchad cmd to install all mason binaries listed
-    vim.api.nvim_create_user_command("MasonInstallAll", function()
-      vim.cmd("MasonInstall " .. table.concat(opts.ensure_installed, " "))
-    end, {})
-
-    vim.g.mason_binaries_list = opts.ensure_installed
   end,
 }

ecfab04e077ab9d228bffef3f971b4dc3d375526
maxcchuang <maxcchuang@google.com>
2024-09-18 09:38:54 +0000

    use c-y to accept cmp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -124,7 +124,7 @@ return {
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
         ["<A-Space>"] = cmp.mapping.complete(),
         ["<C-e>"] = cmp.mapping.abort(),
-        ["<CR>"] = cmp.mapping.confirm {
+        ["<C-y>"] = cmp.mapping.confirm {
           behavior = cmp.ConfirmBehavior.Insert,
           select = true,
         },

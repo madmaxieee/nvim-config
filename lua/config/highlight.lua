3c8947de00fadd3a0a7f24bf4f11e20cb18e89b1
maxcchuang <maxcchuang@google.com>
2024-10-16 20:41:44 +0800

    change VisualMatch color

--- lua/config/highlight.lua
+++ lua/config/highlight.lua
@@ -1,4 +1,12 @@
+local color_utils = require "utils.colors"
+
 local colorscheme_group = vim.api.nvim_create_augroup("CreateCustomHighlight", { clear = true })
+
+local function update_fg_bg()
+  local normal_highlight = vim.api.nvim_get_hl(0, { name = "Normal" })
+  color_utils.fg = normal_highlight.fg
+  color_utils.bg = normal_highlight.bg
+end
 
 local function setup_visual_dimmed()
   local visual_bg = vim.api.nvim_get_hl(0, { name = "Visual" }).bg
@@ -12,6 +20,7 @@ end
 vim.api.nvim_create_autocmd("ColorScheme", {
   group = colorscheme_group,
   callback = function()
+    update_fg_bg()
     setup_visual_dimmed()
   end,
 })
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -153,6 +153,9 @@ return {
     "aaron-p1/match-visual.nvim",
     event = require("utils.events").VisualEnter,
     config = function()
+      local color_utils = require "utils.colors"
+      local bg = color_utils.blend_bg(vim.api.nvim_get_hl(0, { name = "Visual" }).bg, 0.6)
+      vim.api.nvim_set_hl(0, "VisualMatch", { bg = bg })
       require("match-visual").setup {}
     end,
   },

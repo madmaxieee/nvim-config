5af00ef78effd6ea14a84cd2459c1ed571c6460e
maxcchuang <maxcchuang@google.com>
2024-10-24 14:03:20 +0800

    add highlight yanked text

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -64,3 +64,11 @@ autocmd("FocusGained", {
     vim.cmd "checktime"
   end,
 })
+
+autocmd("TextYankPost", {
+  callback = function()
+    vim.highlight.on_yank { higroup = "Visual", timeout = 200 }
+  end,
+  group = vim.api.nvim_create_augroup("YankHighlight", { clear = true }),
+  pattern = "*",
+})

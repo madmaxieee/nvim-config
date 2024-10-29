170992214fb3288e18a7545152858674677ea50b
maxcchuang <maxcchuang@google.com>
2024-10-29 10:21:59 +0000

    bind repeatable ]h for diff view

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -95,10 +95,26 @@ return {
     },
     opts = {
       hooks = {
-        ---@diagnostic disable-next-line: unused-local
         diff_buf_read = function(bufnr)
           vim.opt_local.wrap = false
           vim.opt_local.buflisted = false
+          local map_repeatable_pair = require("utils").map_repeatable_pair
+          map_repeatable_pair({ "n", "x", "o" }, {
+            next = {
+              "]h",
+              function()
+                vim.cmd.normal { "]c", bang = true }
+              end,
+              { desc = "Next hunk", buffer = bufnr },
+            },
+            prev = {
+              "[h",
+              function()
+                vim.cmd.normal { "[c", bang = true }
+              end,
+              { desc = "Previous hunk", buffer = bufnr },
+            },
+          })
         end,
       },
     },

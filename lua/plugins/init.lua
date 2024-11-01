6be60bfefe0543c5b54ca4b930ef2afad8d22ecf
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2024-11-02 01:37:47 +0800

    add loading event for bigfile

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -119,6 +119,7 @@ return {
   },
   {
     "LunarVim/bigfile.nvim",
+    event = "BufReadPre",
     opts = {
       filesize = 2, -- size of the file in MiB, the plugin round file sizes to the closest MiB
       pattern = { "*" }, -- autocmd pattern or function see <### Overriding the detection of big files>

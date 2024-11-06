27916c7d401ddcab0b251d5ceef3632acefb643d
maxcchuang <maxcchuang@google.com>
2024-11-06 12:30:16 +0000

    add marks.nvim

---
+++ lua/plugins/mark.lua
@@ -0,0 +1,36 @@
+return {
+  "chentoast/marks.nvim",
+  event = "VeryLazy",
+  config = function()
+    local marks = require "marks"
+    local marks_utils = require "marks.utils"
+
+    local utils = require "utils"
+    local map = utils.safe_keymap_set
+
+    marks.setup {
+      default_mappings = false,
+      mappings = {
+        set = "m",
+        set_next = "m,",
+        toggle = "m;",
+      },
+    }
+
+    map("n", "dm", function()
+      local input = vim.fn.nr2char(vim.fn.getchar())
+      if input == "-" then
+        marks.delete_line()
+      elseif input == " " then
+        marks.delete_buf()
+      elseif marks_utils.is_valid_mark(input) then
+        marks.mark_state:delete_mark(input)
+      end
+    end, { desc = "delete mark" })
+
+    utils.map_repeatable_pair("n", {
+      next = { "]m", marks.next, { desc = "next mark" } },
+      prev = { "[m", marks.prev, { desc = "previous mark" } },
+    })
+  end,
+}

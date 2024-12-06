bff76881f3ea0f193a4546eb7fc85b7154482ec7
Max Chuang <maxcchuang@google.com>
2024-12-06 19:11:56 +0800

    add hardtime

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -191,4 +191,10 @@ return {
       end, { desc = "delete buffer without disrupting window layout" })
     end,
   },
+  {
+    "m4xshen/hardtime.nvim",
+    event = "VeryLazy",
+    dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+    opts = {},
+  },
 }

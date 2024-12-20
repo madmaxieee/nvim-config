09e9a1208abcc4d7dc97a22be69816ca8f3480bc
Max Chuang <maxcchuang@google.com>
2024-12-20 14:01:27 +0000

    add toggleterm back

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -5,14 +5,14 @@ local terminal = nil
 local lazygit = nil
 
 local function get_terminal_float_opts()
-  return float_window_config(0.8, 0.6, {})
+  return float_window_config(0.8, 0.8, {})
 end
 
 local function get_lazygit_float_opts()
   return float_window_config(0.8, 0.8, {})
 end
 
-local function on_open(toggle_keymap)
+local function set_toggle_keymap(toggle_keymap)
   return function(term)
     map("n", "<C-c>", "<cmd>close<CR>", { buffer = term.bufnr, desc = "close toggleterm" })
     map("t", toggle_keymap, "<cmd>close<CR>", { buffer = term.bufnr, desc = "toggle term" })
@@ -28,8 +28,8 @@ local function create_terminal()
     cmd = vim.o.shell,
     direction = "float",
     hidden = true,
-    float_opts = get_terminal_float_opts(),
-    on_open = on_open "<A-i>",
+    float_opts = float_window_config(0.8, 0.8, {}),
+    on_open = set_toggle_keymap "<A-e>",
   }
 end
 
@@ -40,7 +40,7 @@ local function create_lazygit()
     direction = "float",
     hidden = true,
     float_opts = get_lazygit_float_opts(),
-    on_open = on_open "<A-g>",
+    on_open = set_toggle_keymap "<A-g>",
   }
 end
 
@@ -59,9 +59,24 @@ return {
       end,
       desc = "Toggle lazygit",
     },
+    {
+      "<A-e>",
+      mode = { "i", "n", "t" },
+      function()
+        if terminal then
+          terminal:toggle()
+        else
+          terminal = create_terminal()
+        end
+      end,
+      desc = "Toggle terminal",
+    },
   },
   version = "*",
-  init = function()
+  config = function()
+    terminal = create_terminal()
+    lazygit = create_lazygit()
+
     vim.api.nvim_create_autocmd("VimResized", {
       group = vim.api.nvim_create_augroup("ToggleTermResize", { clear = true }),
       callback = function()
@@ -73,9 +88,5 @@ return {
         end
       end,
     })
-  end,
-  config = function()
-    terminal = create_terminal()
-    lazygit = create_lazygit()
   end,
 }

d44611b5feb84c50d0ef9719a7810963e4b6448e
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2024-12-31 22:25:14 +0800

    add Vs usercmd

--- lua/config/usercmd.lua
+++ lua/config/usercmd.lua
@@ -1,8 +1,12 @@
+-- common typos
 vim.api.nvim_create_user_command("Q", "q<bang>", { bang = true })
 vim.api.nvim_create_user_command("Qa", "qa<bang>", { bang = true })
 vim.api.nvim_create_user_command("W", "w", {})
-vim.api.nvim_create_user_command("WW", "noa w", {})
 vim.api.nvim_create_user_command("Wq", "wq", {})
 vim.api.nvim_create_user_command("Wqa", "wqa", {})
+vim.api.nvim_create_user_command("Vs", "vs", {})
+
+-- save without auto format
+vim.api.nvim_create_user_command("WW", "noa w", {})
 
 vim.api.nvim_create_user_command("SourceThis", "source %", {})

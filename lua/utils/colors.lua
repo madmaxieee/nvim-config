39afab27a7d16a355e103056ecd00371c720ef91
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2024-12-31 23:03:10 +0800

    add color utils

--- lua/utils/colors.lua
+++ lua/utils/colors.lua
@@ -2,8 +2,10 @@
 
 local M = {}
 
+---@alias RGB [number, number, number]
 ---@param c  string|number
-local function rgb(c)
+---@return RGB
+function M.rgb(c)
   if type(c) == "number" then
     return { c / (256 ^ 2), (c % (256 ^ 2)) / 256, c % 256 }
   else
@@ -12,13 +14,22 @@ local function rgb(c)
   end
 end
 
+---@param color RGB|number
+---@return string
+function M.hex(color)
+  if type(color) == "number" then
+    color = M.rgb(color)
+  end
+  return string.format("#%02x%02x%02x", color[1], color[2], color[3])
+end
+
 ---@param foreground string foreground color
 ---@param background string background color
 ---@param alpha number|string number between 0 and 1. 0 results in bg, 1 results in fg
 function M.blend(foreground, alpha, background)
   alpha = type(alpha) == "string" and (tonumber(alpha, 16) / 0xff) or alpha
-  local bg = rgb(background)
-  local fg = rgb(foreground)
+  local bg = M.rgb(background)
+  local fg = M.rgb(foreground)
 
   local blendChannel = function(i)
     local ret = (alpha * fg[i] + ((1 - alpha) * bg[i]))
@@ -28,13 +39,13 @@ function M.blend(foreground, alpha, background)
   return string.format("#%02x%02x%02x", blendChannel(1), blendChannel(2), blendChannel(3))
 end
 
-function M.blend_bg(hex, amount, bg)
-  return M.blend(hex, amount, bg or M.bg)
+function M.blend_bg(color, amount)
+  return M.blend(color, amount, M.bg)
 end
 M.darken = M.blend_bg
 
-function M.blend_fg(hex, amount, fg)
-  return M.blend(hex, amount, fg or M.fg)
+function M.blend_fg(color, amount)
+  return M.blend(color, amount, M.fg)
 end
 M.lighten = M.blend_fg
 

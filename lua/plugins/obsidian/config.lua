d70d1016aadea17e2e4beaf617aed07dc4c31393
Max Chuang <maxcchuang@google.com>
2025-01-08 22:20:37 +0800

    fix obsidian save on leave

--- lua/plugins/obsidian/config.lua
+++ lua/plugins/obsidian/config.lua
@@ -64,10 +64,10 @@ local auto_save_timer = assert(vim.uv.new_timer(), "Failed to create timer")
 function M.setup_auto_commit(opts)
   local auto_save_interval = opts.auto_save_interval * 1000
   local commit_interval = opts.commit_interval
-  local vault_folder = opts.vault_folder
+  local vault_folder = vim.fn.fnameescape(opts.vault_folder)
 
   local function git(args)
-    local command = ("git -C '" .. vault_folder .. "' " .. args)
+    local command = "git -C '" .. vault_folder .. "' " .. args
     local handle = io.popen(command)
     if handle then
       local result = handle:read "*a"
@@ -76,17 +76,22 @@ function M.setup_auto_commit(opts)
     end
   end
 
-  local function try_commit(interval)
+  ---@param message string?: the commit message
+  ---@param write_all boolean?: the commit message
+  local function try_commit(message, write_all)
     local commit_time_str = git "log -1 --format=%ct"
     local commit_time = tonumber(commit_time_str)
     if commit_time == nil then
       return
     end
+    local commit_message = message and vim.fn.escape(message, [['\]]) or "auto commit"
     local current_time = os.time()
-    if current_time - commit_time >= interval then
-      vim.cmd "wa"
+    if current_time - commit_time >= commit_interval then
+      if write_all then
+        vim.cmd "wa"
+      end
       git "add --all"
-      git "commit -m 'auto commit'"
+      git("commit  -m '" .. commit_message .. "'")
       git "push --quiet"
     end
   end
@@ -112,21 +117,22 @@ function M.setup_auto_commit(opts)
         end
         if not auto_save_timer:is_active() then
           auto_save_timer:start(auto_save_interval, 0, function()
-            -- can't call vim api's like vim.cmd directly in uv callbacks
+            -- can't call vim api's like vim.cmd in uv callback
             vim.schedule(function()
-              try_commit(commit_interval)
+              try_commit("auto commit: inactive", true)
             end)
           end)
         end
       end,
     })
-    vim.api.nvim_create_autocmd({ "VimLeave" }, {
+    vim.api.nvim_create_autocmd({ "VimLeavePre" }, {
       group = auto_save_group,
       callback = function()
         if vim.fn.getcwd() ~= vault_folder then
           return
         end
-        try_commit(commit_interval)
+        -- can't call vim.cmd here, not sure why
+        try_commit "auto commit: leave"
       end,
     })
   end

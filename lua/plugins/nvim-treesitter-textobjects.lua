37e995097c57af6176961b1c92033d2a2577f26e
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2025-01-14 21:37:09 +0800

    refactor treesitter textobject keymaps

--- lua/plugins/nvim-treesitter-textobjects.lua
+++ lua/plugins/nvim-treesitter-textobjects.lua
@@ -9,53 +9,78 @@ return {
         select = {
           enable = true,
           lookahead = false,
-          keymaps = {
-            ["aa"] = "@parameter.outer",
-            ["ia"] = "@parameter.inner",
-            ["af"] = "@function.outer",
-            ["if"] = "@function.inner",
-            ["ac"] = "@class.outer",
-            ["ic"] = "@class.inner",
-          },
-          selection_modes = {
-            ["@parameter.outer"] = "v",
-            ["@parameter.inner"] = "v",
-            ["@function.outer"] = "v",
-            ["@function.inner"] = "v",
-            ["@class.outer"] = "v",
-            ["@class.inner"] = "v",
-          },
           include_surrounding_whitespace = false,
         },
         move = {
           enable = true,
           set_jumps = true,
-          goto_next_start = {
-            ["]f"] = "@function.outer",
-            ["]]"] = "@class.outer",
-            ["]o"] = "@loop.*",
-            ["]s"] = { query = "@scope", query_group = "locals", desc = "Next scope" },
-            ["]z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
-          },
-          goto_next_end = {
-            ["]F"] = "@function.outer",
-            ["]["] = "@class.outer",
-          },
-          goto_previous_start = {
-            ["[f"] = "@function.outer",
-            ["[["] = "@class.outer",
-            ["[o"] = "@loop.*",
-            ["[s"] = { query = "@scope", query_group = "locals", desc = "Next scope" },
-            ["[z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
-          },
-          goto_previous_end = {
-            ["[F"] = "@function.outer",
-            ["[]"] = "@class.outer",
-          },
         },
         swap = { enable = false },
         lsp_interop = { enable = false },
       },
     }
+
+    local utils = require "utils"
+
+    local map = utils.safe_keymap_set
+    local select = require "nvim-treesitter.textobjects.select"
+    local select_keymaps = {
+      ["aa"] = "@parameter.outer",
+      ["ia"] = "@parameter.inner",
+      ["af"] = "@function.outer",
+      ["if"] = "@function.inner",
+      ["ac"] = "@class.outer",
+      ["ic"] = "@class.inner",
+    }
+    for keymap, query in pairs(select_keymaps) do
+      map( --
+        { "x", "o" },
+        keymap,
+        function()
+          select.select_textobject(query, "textobjects", "v")
+        end,
+        { desc = "select " .. query }
+      )
+    end
+
+    local map_repeatable_pair = utils.map_repeatable_pair
+    local move = require "nvim-treesitter.textobjects.move"
+    local move_keymaps = {
+      {
+        next_key = "]f",
+        prev_key = "[f",
+        query = "@function.outer",
+      },
+      {
+        next_key = "]]",
+        prev_key = "[[",
+        query = "@class.outer",
+      },
+      {
+        next_key = "]z",
+        prev_key = "[z",
+        query = "@fold",
+        query_group = "folds",
+      },
+    }
+    for _, keymap in ipairs(move_keymaps) do
+      keymap.query_group = keymap.query_group or "textobjects"
+      map_repeatable_pair({ "n", "x", "o" }, {
+        next = {
+          keymap.next_key,
+          function()
+            move.goto_next_start(keymap.query, keymap.query_group)
+          end,
+          { desc = "next " .. keymap.query },
+        },
+        prev = {
+          keymap.prev_key,
+          function()
+            move.goto_previous_start(keymap.query, keymap.query_group)
+          end,
+          { desc = "prev " .. keymap.query },
+        },
+      })
+    end
   end,
 }

82df79ca26cb05ce3a1b7cecd9be425b9bb2bf56
Max Chuang <maxcchuang@google.com>
2025-01-16 17:08:13 +0800

    enable dropbar even if there is no treesitter or lsp

--- lua/plugins/dropbar.lua
+++ lua/plugins/dropbar.lua
@@ -27,5 +27,23 @@ return {
         end,
       },
     },
+    bar = {
+      enable = function(buf, win, _)
+        if
+          not vim.api.nvim_buf_is_valid(buf)
+          or not vim.api.nvim_win_is_valid(win)
+          or vim.fn.win_gettype(win) ~= ""
+          or vim.wo[win].winbar ~= ""
+          or vim.bo[buf].ft == "help"
+        then
+          return false
+        end
+        local stat = vim.uv.fs_stat(vim.api.nvim_buf_get_name(buf))
+        if stat and stat.size > 1024 * 1024 then
+          return false
+        end
+        return true
+      end,
+    },
   },
 }

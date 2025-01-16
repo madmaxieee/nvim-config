db7cbaaf8384b9f0bd48f27ec45cb04ce8289ce9
Max Chuang <maxcchuang@google.com>
2025-01-17 02:28:00 +0800

    move to mini.surround

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -6,4 +6,24 @@ return {
       require("mini.pairs").setup()
     end,
   },
+
+  {
+    "echasnovski/mini.surround",
+    event = "VeryLazy",
+    opts = {
+      mappings = {
+        add = "yz", -- Add surrounding in Normal and Visual modes
+        delete = "dz", -- Delete surrounding
+        find = "", -- Find surrounding (to the right)
+        find_left = "", -- Find surrounding (to the left)
+        highlight = "", -- Highlight surrounding
+        replace = "cz", -- Replace surrounding
+        update_n_lines = "", -- Update `n_lines`
+
+        suffix_last = "", -- Suffix to search with "prev" method
+        suffix_next = "", -- Suffix to search with "next" method
+      },
+      n_lines = 20,
+    },
+  },
 }

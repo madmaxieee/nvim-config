b90bbde7096e473e370df8ed1beaf521f8ba032f
Max Chuang <maxcchuang@google.com>
2025-01-16 17:47:14 +0800

    refactor

--- lua/plugins/smart-splits.lua
+++ lua/plugins/smart-splits.lua
@@ -17,53 +17,12 @@ return {
       end,
       desc = "resize right",
     },
-    {
-      "<leader>H",
-      mode = "n",
-      function()
-        require("smart-splits").swap_buf_left()
-        require("smart-splits").move_cursor_left()
-      end,
-      desc = "swap buffer left",
-    },
-    {
-      "<leader>J",
-      mode = "n",
-      function()
-        require("smart-splits").swap_buf_down()
-        require("smart-splits").move_cursor_down()
-      end,
-      desc = "swap buffer down",
-    },
-    {
-      "<leader>K",
-      mode = "n",
-      function()
-        require("smart-splits").swap_buf_up()
-        require("smart-splits").move_cursor_up()
-      end,
-      desc = "swap buffer up",
-    },
-    {
-      "<leader>L",
-      mode = "n",
-      function()
-        require("smart-splits").swap_buf_right()
-        require("smart-splits").move_cursor_right()
-      end,
-      desc = "swap buffer right",
-    },
   },
   opts = {
     default_amount = 5,
     resize_mode = {
-      quit_key = "<ESC>",
       resize_keys = { "h", "j", "k", "l" },
       silent = true,
-      hooks = {
-        on_enter = nil,
-        on_leave = nil,
-      },
     },
   },
 }

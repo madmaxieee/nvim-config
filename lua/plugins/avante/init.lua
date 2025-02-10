5e7f169b424d267e481178587843e35e72524fb2
Max Chuang <maxcchuang@google.com>
2025-02-10 20:42:16 +0800

    promote avante to main config

---
+++ lua/plugins/avante/init.lua
@@ -0,0 +1,166 @@
+local password_store_path = "openai/avante"
+
+local function setup_avante(api_key)
+  local api_key_cmd = { "echo", api_key }
+  local opts = {
+    provider = "openai",
+    openai = {
+      model = "gpt-4o",
+      api_key_name = api_key_cmd,
+    },
+    behaviour = {
+      auto_suggestions = false,
+      auto_set_highlight_group = true,
+      auto_set_keymaps = false,
+      auto_apply_diff_after_generation = false,
+      support_paste_from_clipboard = false,
+      minimize_diff = true,
+    },
+    mappings = {
+      diff = {
+        ours = "co",
+        theirs = "ct",
+        all_theirs = "ca",
+        both = "cb",
+        cursor = "cc",
+        next = "]h",
+        prev = "[h",
+      },
+      jump = {
+        next = "]]",
+        prev = "[[",
+      },
+      submit = {
+        normal = "<CR>",
+        insert = "<C-s>",
+      },
+      sidebar = {
+        apply_all = "A",
+        apply_cursor = "a",
+        switch_windows = "<Tab>",
+        reverse_switch_windows = "<S-Tab>",
+      },
+    },
+    hints = { enabled = false },
+  }
+  require("avante").setup(opts)
+  vim.g.avante_loaded = true
+end
+
+local function prompt_and_setup_avante(callback)
+  if vim.g.avante_loaded then
+    return
+  end
+
+  local SecretInput = require "plugins.avante.secret_input"
+  local event = require("nui.utils.autocmd").event
+
+  local input = SecretInput({
+    relative = "editor",
+    position = "50%",
+    size = { width = 40 },
+    border = {
+      style = "rounded",
+      text = {
+        top = "[unlock password store]",
+        top_align = "center",
+      },
+    },
+  }, {
+    on_submit = function(value)
+      local output = vim
+        .system({ "pass", password_store_path }, {
+          env = { PASSWORD_STORE_GPG_OPTS = "--passphrase-fd 0 --pinentry-mode loopback" },
+          stdin = value,
+          text = true,
+        })
+        :wait()
+      if output.code == 0 then
+        setup_avante(output.stdout)
+        callback()
+      else
+        vim.notify("Can't unlock password store", vim.log.levels.ERROR)
+      end
+    end,
+  })
+
+  input:mount()
+
+  input:map("n", "<Esc>", function()
+    input:unmount()
+  end, { noremap = true })
+
+  input:on(event.BufLeave, function()
+    input:unmount()
+  end)
+end
+
+return {
+  {
+    cond = not vim.g.minimal_mode,
+    "yetone/avante.nvim",
+    build = "make",
+    cmd = { "Avante" },
+    keys = {
+      {
+        "<leader>aa",
+        mode = "n",
+        function()
+          if vim.g.avante_loaded then
+            require("avante.api").ask()
+          else
+            prompt_and_setup_avante(require("avante.api").ask)
+          end
+        end,
+        desc = "avante ask",
+      },
+      {
+        "<leader>ae",
+        mode = "v",
+        function()
+          if vim.g.avante_loaded then
+            require("avante.api").edit()
+          else
+            prompt_and_setup_avante(require("avante.api").edit)
+          end
+        end,
+        desc = "avante edit",
+      },
+    },
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "nvim-tree/nvim-web-devicons",
+      {
+        "MeanderingProgrammer/render-markdown.nvim",
+        opts = { file_types = { "Avante" } },
+        ft = { "Avante" },
+      },
+    },
+
+    config = function()
+      local check_key_output = vim
+        .system({ "pass", password_store_path }, {
+          env = { PASSWORD_STORE_GPG_OPTS = "--pinentry-mode cancel" },
+          text = true,
+        })
+        :wait()
+      if check_key_output.code == 0 then
+        setup_avante(check_key_output.stdout)
+      end
+    end,
+  },
+
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = {
+      options = {
+        disabled_filetypes = {
+          statusline = { "Avante" },
+        },
+      },
+    },
+  },
+}
---
+++ lua/plugins/avante/secret_input.lua
@@ -0,0 +1,40 @@
+local Input = require "nui.input"
+
+---@diagnostic disable-next-line: undefined-field
+local SecretInput = Input:extend "SecretInput"
+
+function SecretInput:init(popup_options, options)
+  assert(
+    not options.conceal_char or vim.api.nvim_strwidth(options.conceal_char) == 1,
+    "conceal_char must be a single char"
+  )
+
+  popup_options.win_options = vim.tbl_deep_extend("force", popup_options.win_options or {}, {
+    conceallevel = 2,
+    concealcursor = "nvi",
+  })
+
+  SecretInput.super.init(self, popup_options, options)
+
+  self._.conceal_char = type(options.conceal_char) == "nil" and "*" or options.conceal_char
+end
+
+function SecretInput:mount()
+  SecretInput.super.mount(self)
+
+  local conceal_char = self._.conceal_char
+  local prompt_length = vim.api.nvim_strwidth(vim.fn.prompt_getprompt(self.bufnr))
+
+  vim.api.nvim_buf_call(self.bufnr, function()
+    vim.cmd(string.format(
+      [[
+        syn region SecretValue start=/^/ms=s+%s end=/$/ contains=SecretChar
+        syn match SecretChar /./ contained conceal %s
+      ]],
+      prompt_length,
+      conceal_char and "cchar=" .. (conceal_char or "*") or ""
+    ))
+  end)
+end
+
+return SecretInput

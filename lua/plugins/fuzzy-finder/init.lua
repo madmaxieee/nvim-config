df35aa1121f1d74ff57b43467c80c7ac281f7212
Max Chuang <maxcchuang@google.com>
2025-08-04 15:00:03 +0800

    install fff.nvim

--- lua/plugins/fuzzy-finder/init.lua
+++ lua/plugins/fuzzy-finder/init.lua
@@ -3,4 +3,20 @@ require("plugins.fuzzy-finder.keymaps").setup()
 return {
   require "plugins.fuzzy-finder.telescope",
   require "plugins.fuzzy-finder.fzf",
+  {
+    "madmaxieee/fff.nvim",
+    branch = "manage-openssl-with-cargo",
+    -- "dmtrKovalenko/fff.nvim",
+    build = "cargo build --release",
+    keys = {
+      {
+        "ff",
+        function()
+          require("fff").find_files()
+        end,
+        desc = "Open file picker",
+      },
+    },
+    opts = {},
+  },
 }

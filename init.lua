7d85e2eb1cfe64c4ce642e53d07dbf5bcf13efa3
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2025-10-08 00:30:58 +0800

    upgrade some plugins

--- init.lua
+++ init.lua
@@ -1,9 +1,3 @@
--- allow neovim to access the lua modules installed via luarocks
-package.path = package.path
-  .. ";"
-  .. (vim.fn.expand "$HOME" .. "/.luarocks/share/lua/5.1/?/init.lua;")
-  .. (vim.fn.expand "$HOME" .. "/.luarocks/share/lua/5.1/?.lua;")
-
 require "config.opts"
 require "config.autocmd"
 

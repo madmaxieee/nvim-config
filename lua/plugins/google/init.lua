bafc13bff9746ca5e1f935878ceaec6a28f99d2f
Max Chuang <maxcchuang@google.com>
2025-10-21 03:09:12 +0000

    chore: Refactor ciderlsp LSP setup for lazy loading
    - Wrapped ciderlsp LSP configuration in utils.on_load for proper dependency handling.
    - Structured ciderlsp.lua as a Lazy.nvim spec file.
    - Removed redundant ciderlsp loading from init.lua.

--- lua/plugins/google/init.lua
+++ lua/plugins/google/init.lua
@@ -2,12 +2,6 @@ if vim.fn.isdirectory "/google/bin" ~= 1 then
   return {}
 end
 
-local utils = require "utils"
-
-utils.on_load("nvim-lspconfig", function()
-  require "plugins.google.ciderlsp"
-end)
-
 return {
   {
     import = "plugins.google",

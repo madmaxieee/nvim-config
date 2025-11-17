08629c1fbccc7a7e2196cf41aba74c619eef2d29
maxcchuang <maxcchuang@google.com>
2025-11-17 15:38:04 +0800

    refactor(lsp): Inline trouble calls in keymap setup
    Removed the local variable assignment for `trouble.open` and replaced
    it with direct calls to `require("trouble").open` within the LSP keymaps
    for `gd`, `gD`, `gr`, and `gi`. This simplifies the keymap file by
    removing an unnecessary local variable.

--- lua/plugins/lsp/keymaps.lua
+++ lua/plugins/lsp/keymaps.lua
@@ -2,14 +2,11 @@ local M = {}
 
 local utils = require "utils"
 local map = utils.safe_keymap_set
-local lsp_utils = require "plugins.lsp.utils"
 
 function M.set_keymaps(bufnr)
   if vim.b[bufnr].lsp_keymaps_set then
     return
   end
-
-  local trouble_open = require("trouble").open
 
   map("n", "K", function()
     vim.lsp.buf.hover()
@@ -17,22 +14,22 @@ function M.set_keymaps(bufnr)
 
   map("n", "gd", function()
     -- vim.lsp.buf.definition()
-    trouble_open { mode = "lsp_definitions" }
+    require("trouble").open { mode = "lsp_definitions" }
   end, { buffer = bufnr, desc = "Go to definition" })
 
   map("n", "gD", function()
     -- vim.lsp.buf.type_definition()
-    trouble_open { mode = "lsp_type_definitions" }
+    require("trouble").open { mode = "lsp_type_definitions" }
   end, { buffer = bufnr, desc = "Go to type definition" })
 
   map("n", "gr", function()
     -- vim.lsp.buf.references()
-    trouble_open { mode = "lsp_references" }
+    require("trouble").open { mode = "lsp_references" }
   end, { buffer = bufnr, desc = "LSP references" })
 
   map("n", "gi", function()
     -- vim.lsp.buf.implementation()
-    trouble_open { mode = "lsp_implementations" }
+    require("trouble").open { mode = "lsp_implementations" }
   end, { buffer = bufnr, desc = "Go to implementation" })
 
   map("n", "<leader>ls", function()

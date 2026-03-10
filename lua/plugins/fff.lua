511ee5769d1b0ba88e618bae7f4d4ad522706893
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2026-03-11 00:22:05 +0800

    chore: Add picker switching to fff-snacks
    Implement a mechanism to toggle between find files and live grep
    pickers using `<S-Tab>`, preserving the current search query for a
    more seamless navigation experience.
    
    Key changes:
    - Define configuration for find-files and live-grep pickers.
    - Add custom actions to `snacks.nvim` to handle picker transitions.
    - Map `<S-Tab>` to trigger transitions in both search modes.
    - Update `fff-snacks` keybindings to utilize the new options.

--- lua/plugins/fff.lua
+++ lua/plugins/fff.lua
@@ -1,3 +1,33 @@
+---@module 'snacks''
+
+local find_files_opts = {
+  win = {
+    input = {
+      keys = {
+        ["<S-Tab>"] = {
+          "go_to_live_grep",
+          mode = { "n", "i" },
+          nowait = true,
+        },
+      },
+    },
+  },
+}
+
+local live_grep_opts = {
+  win = {
+    input = {
+      keys = {
+        ["<S-Tab>"] = {
+          "go_to_find_files",
+          mode = { "n", "i" },
+          nowait = true,
+        },
+      },
+    },
+  },
+}
+
 return {
   {
     "dmtrKovalenko/fff.nvim",
@@ -14,14 +44,14 @@ return {
       {
         "<leader>ff",
         function()
-          require("fff-snacks").find_files()
+          require("fff-snacks").find_files(find_files_opts)
         end,
         desc = "FFF find files",
       },
       {
         "<leader>fw",
         function()
-          require("fff-snacks").live_grep()
+          require("fff-snacks").live_grep(live_grep_opts)
         end,
         desc = "FFF live grep",
       },
@@ -41,6 +71,32 @@ return {
           })
         end,
         desc = "FFF live grep (fuzzy)",
+      },
+    },
+  },
+
+  {
+    "folke/snacks.nvim",
+    ---@module 'snacks'
+    ---@type snacks.Config
+    opts = {
+      input = { enabled = true },
+      picker = {
+        actions = {
+          -- TODO: figure out why toggling to find_files set input to insert mode
+          -- but live grep to normal mode
+          -- TODO: move these actions to fff-snacks plugin
+          go_to_live_grep = function(picker)
+            picker:close()
+            live_grep_opts.search = picker.input.filter.search
+            require("fff-snacks").live_grep(live_grep_opts)
+          end,
+          go_to_find_files = function(picker)
+            picker:close()
+            find_files_opts.search = picker.input.filter.search
+            require("fff-snacks").find_files(find_files_opts)
+          end,
+        },
       },
     },
   },

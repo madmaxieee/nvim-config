ae9bfc735d3acb7c19737b9ae8955a44a782f630
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2026-04-19 14:36:57 +0800

    refactor(blink): simplify pairs config to function-based setup

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -72,143 +72,25 @@ return {
     "saghen/blink.pairs",
     build = "nix run .#build-plugin",
     event = { "InsertEnter", "CmdlineEnter" },
-    --- @module 'blink.pairs'
-    --- @type blink.pairs.Config
-    opts = {
-      mappings = {
-        enabled = true,
-        cmdline = true,
-        pairs = {
-          ["!"] = {
-            {
-              "<!--",
-              "-->",
-              languages = { "html", "markdown", "markdown_inline" },
-            },
-          },
-          ["("] = ")",
-          ["["] = "]",
-          ["{"] = "}",
-          ["'"] = {
-            { -- new
-              "''",
-              "''",
-              when = function(ctx)
-                return ctx:text_before_cursor(1) == "'"
-              end,
-              languages = { "nix" },
-            },
-            {
-              "'''",
-              when = function(ctx)
-                return ctx:text_before_cursor(2) == "''"
-              end,
-              languages = {
-                "python",
-                "toml", -- new
-              },
-            },
-            {
-              "'",
-              enter = false,
-              space = false,
-              when = function(ctx)
-                return ctx.ft ~= "plaintext"
-                  and not ctx.char_under_cursor:match("%w")
-                  and ctx.ts:blacklist("singlequote").matches
-              end,
-            },
-          },
-          ['"'] = {
-            {
-              'r#"',
-              '"#',
-              languages = { "rust" },
-              priority = 100,
-            },
-            {
-              '"""',
-              when = function(ctx)
-                return ctx:text_before_cursor(2) == '""'
-              end,
-              languages = {
-                "python",
-                "elixir",
-                "julia",
-                "kotlin",
-                "scala",
-                "toml", -- new
-              },
-            },
-            { '"', enter = false, space = false },
-          },
-          ["`"] = {
-            {
-              "```",
-              when = function(ctx)
-                return ctx:text_before_cursor(2) == "``"
-              end,
-              languages = {
-                "markdown",
-                "markdown_inline",
-                "typst",
-                "vimwiki",
-                "rmarkdown",
-                "rmd",
-                "quarto",
-              },
-            },
-            {
-              "`",
-              "'",
-              languages = { "bibtex", "latex", "plaintex" },
-            },
-            { "`", enter = false, space = false },
-          },
-          ["_"] = {
-            {
-              "_",
-              when = function(ctx)
-                return not ctx.char_under_cursor:match("%w")
-                  and ctx.ts:blacklist("underscore").matches
-              end,
-              languages = { "typst" },
-            },
-          },
-          ["*"] = {
-            {
-              "*",
-              when = function(ctx)
-                return ctx.ts:blacklist("asterisk").matches
-              end,
-              languages = { "typst" },
-            },
-          },
-          ["<"] = {
-            {
-              "<",
-              ">",
-              when = function(ctx)
-                return ctx.ts:whitelist("angle").matches
-              end,
-              languages = { "rust" },
-            },
-          },
-          ["$"] = {
-            {
-              "$",
-              languages = {
-                "markdown",
-                "markdown_inline",
-                "typst",
-                "latex",
-                "plaintex",
-              },
-            },
-          },
-        },
-      },
-      highlights = { enabled = false },
-    },
+    config = function()
+      local opts = {
+        highlights = { enabled = false },
+        mappings = require("blink.pairs.config.mappings").default,
+      }
+      -- TODO: open a PR to blink.pairs
+      table.insert(
+        ---@diagnostic disable-next-line: param-type-mismatch
+        opts.mappings.pairs["'"],
+        ---@type blink.pairs.RuleDefinition
+        {
+          "''",
+          when = function(ctx)
+            return ctx:text_before_cursor(1) == "'"
+          end,
+          languages = { "nix" },
+        }
+      )
+      require("blink.pairs").setup(opts)
+    end,
   },
 }

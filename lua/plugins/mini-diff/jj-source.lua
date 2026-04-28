0e6ebc11ddefbfbca569f0ec9bc3141b9499251e
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2026-04-28 11:36:09 +0800

    refactor(mini-diff): refactor jj-source async logic
    Refactor async_get_ref_text to use co2.lua, eliminating callback nesting. 
    Added logic to correctly differentiate between new and ignored files by 
    checking the working copy if the base revision check fails.

--- lua/plugins/mini-diff/jj-source.lua
+++ lua/plugins/mini-diff/jj-source.lua
@@ -1,6 +1,7 @@
 local find_jj_root = require("utils.jj").find_root
 local jj_config = require("utils.jj").config
 local make_source = require("plugins.mini-diff.make-source")
+local co2 = require("co2")
 
 local function jj_cmd(...)
   local JJ = {
@@ -25,10 +26,12 @@ local jj_opts = {
 
   get_root = find_jj_root,
 
-  async_get_ref_text = function(path, callback)
+  async_get_ref_text = co2.wrap(function(ctx, path, callback)
     local dir = vim.fs.dirname(path)
     local file = vim.fs.basename(path)
-    vim.system(
+
+    local res = ctx.await(
+      vim.system,
       jj_cmd(
         "--ignore-working-copy",
         "file",
@@ -38,17 +41,26 @@ local jj_opts = {
         "--",
         file
       ),
-      { cwd = dir },
-      function(res)
-        if res.code ~= 0 then
-          callback("")
-          return
-        end
-        local output = res.stdout or ""
-        callback(output)
-      end
+      { cwd = dir }
     )
-  end,
+
+    if res.code == 0 then
+      local output = res.stdout or ""
+      callback(output)
+      return
+    end
+
+    -- If missing from base revision, check working copy (@) to differentiate
+    -- between new files (diff against empty string) and ignored files (do nothing).
+    local res2 = ctx.await(
+      vim.system,
+      jj_cmd("file", "show", "-r", "@", "--", file),
+      { cwd = dir }
+    )
+    if res2.code == 0 then
+      callback("")
+    end
+  end),
 }
 
 return function()

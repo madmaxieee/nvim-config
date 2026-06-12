0daa4c51957887e9c28b62809608283a9751d3f0
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2026-06-12 10:08:10 +0000

    feat(smart-gf): support //depot/ paths
    - Support resolving paths starting with "//depot/".
    - Calculate the depot root relative to the google3 root.

--- plugin/google/smart-gf.lua
+++ plugin/google/smart-gf.lua
@@ -6,21 +6,30 @@ local map = require("utils").safe_keymap_set
 local gutils = require("gutils")
 
 local G3_PREFIX = "google3/"
+local DEPOT_PREFIX = "//depot/"
 
 map({ "n", "x" }, "gf", function()
   local path
   local mode = vim.fn.mode()
   if mode == "v" or mode == "V" or mode == "\x16" then
-    path = table.concat(vim.fn.getregion(vim.fn.getpos("v"), vim.fn.getpos("."), { type = mode }), "\n")
+    path = table.concat(
+      vim.fn.getregion(vim.fn.getpos("v"), vim.fn.getpos("."), { type = mode }),
+      "\n"
+    )
     path = path:gsub("^%s*(.-)%s*$", "%1")
   else
     path = vim.fn.expand("<cfile>")
   end
 
+  local g3_root = gutils.get_google3_root(vim.fn.getcwd())
+  local depot_root = vim.fs.normalize(vim.fs.joinpath(g3_root, ".."))
+
   if path:find(G3_PREFIX, nil, true) == 1 then
-    local g3_root = gutils.get_google3_root(vim.fn.getcwd())
     path = path:sub(#G3_PREFIX + 1)
     path = vim.fs.joinpath(g3_root, path)
+  elseif path:find(DEPOT_PREFIX, nil, true) == 1 then
+    path = path:sub(#DEPOT_PREFIX + 1)
+    path = vim.fs.joinpath(depot_root, path)
   end
 
   if vim.uv.fs_stat(path) then

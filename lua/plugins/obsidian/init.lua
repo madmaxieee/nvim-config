3150c72b1c37848cfd564d2f5da0bb9814c475e5
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2026-07-17 13:06:08 +0800

    refactor(obsidian): update obsidian.nvim and simplify configuration
    - Update obsidian.nvim to the latest version.
    - Clean up obsidian.nvim configuration by removing custom note_id_func, completion settings, and legacy autocommands.
    - Set vim.opt.conceallevel to 2.
    - Add <leader>fo keybinding for Obsidian search.

--- lua/plugins/obsidian/init.lua
+++ lua/plugins/obsidian/init.lua
@@ -1,5 +1,6 @@
 local vault_folder = vim.fn.resolve(vim.fn.expand("~/obsidian"))
 
+---@type LazySpec
 return {
   {
     cond = vim.startswith(vim.uv.cwd() or "", vault_folder),
@@ -7,67 +8,31 @@ return {
     version = "*",
     lazy = false,
     dependencies = { "nvim-lua/plenary.nvim" },
+
+    keys = {
+      {
+        "<leader>fo",
+        "<cmd>Obsidian search<cr>",
+        desc = "Obsidian search",
+      },
+    },
+
     opts = {
+      legacy_commands = false,
       workspaces = {
         {
           name = "notes",
           path = "~/obsidian/notes",
         },
       },
-      completion = {
-        blink = true,
-        min_chars = 2,
-      },
       picker = {
         name = "snacks.pick",
       },
-      ---@param title string|?
-      ---@return string
-      note_id_func = function(title)
-        -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
-        local suffix = ""
-        if title ~= nil then
-          -- If title is given, transform it into valid file name.
-          suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
-        else
-          -- If title is nil, just add 4 random uppercase letters to the suffix.
-          for _ = 1, 4 do
-            suffix = suffix .. string.char(math.random(65, 90))
-          end
-        end
-        return tostring(os.time()) .. "-" .. suffix
-      end,
-      legacy_commands = false,
     },
 
     init = function()
-      local map = require("utils").safe_keymap_set
-      vim.api.nvim_create_autocmd("User", {
-        pattern = "ObsidianNoteEnter",
-        callback = function(opts)
-          vim.wo.conceallevel = 1
-          map("n", "<cr>", function()
-            require("obsidian").util.toggle_checkbox()
-          end, {
-            buffer = opts.buf,
-            desc = "Toggle checkbox",
-          })
-        end,
-      })
-      map("n", "<leader>fo", "<cmd>Obsidian search<cr>", {})
+      vim.opt.conceallevel = 2
       vim.cmd.cabbrev("O", "Obsidian")
-
-      -- NOTE: typos_lsp for some reason would attach to the unnamed buffer
-      -- when obsidian is active, causing errors
-      local lsp_utils = require("plugins.lsp.utils")
-      lsp_utils.lsp_disable("typos_lsp")
-      vim.api.nvim_create_autocmd("User", {
-        pattern = "VeryLazy",
-        once = true,
-        callback = function()
-          lsp_utils.lsp_enable("typos_lsp")
-        end,
-      })
     end,
 
     config = function(_, opts)

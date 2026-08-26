13ffa88092f87de2b223a53a9ecb701f015fd6fb
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2026-08-26 05:23:41 +0000

    feat: add :JJDiffTrunk and :JJDiffReset commands

--- lua/plugins/jj-diff.lua
+++ lua/plugins/jj-diff.lua
@@ -6,4 +6,16 @@ return {
     "JJDiffIncludeParent",
     "JJDiffExcludeParent",
   },
+  init = function()
+    vim.api.nvim_create_user_command(
+      "JJDiffTrunk",
+      "JJDiff trunk()",
+      { desc = "Set diff base to `trunk()`" }
+    )
+    vim.api.nvim_create_user_command(
+      "JJDiffReset",
+      "JJDiff @-",
+      { desc = "Reset diff base to the default (`@-`)" }
+    )
+  end,
 }
